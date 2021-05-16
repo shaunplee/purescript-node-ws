4c79aacf8891a4c6cdcd6824d12a64f8db26244a
Shaun Lee <shaun@curlyfri.es>
2021-05-15 17:08:51 -0700

    Make it compile on purs 0.14.1.

--- src/WebSocket/Ws.purs
+++ src/WebSocket/Ws.purs
@@ -1,21 +1,25 @@
 module WebSocket.Ws where
-import Prelude
 
-import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Exception (Error)
-import Control.Monad.Eff.Uncurried (EffFn1, EffFn2, EffFn3, mkEffFn1, mkEffFn2, runEffFn2, runEffFn3)
+import Prelude
+import Effect (Effect)
+import Effect.Exception (Error)
+import Effect.Uncurried (EffectFn1, EffectFn2, EffectFn3, mkEffectFn1, mkEffectFn2, runEffectFn2, runEffectFn3)
 import Data.Newtype (class Newtype)
-import Data.Record (insert)
+import Record (insert)
 import Data.Symbol (SProxy(..))
 import Node.HTTP (Request, Server)
-import Type.Row (class RowLacks)
+import Type.Row (class Cons, class Lacks, class Union)
 
 -- | The type of a WebSocket server object
 foreign import data WebSocketServer :: Type
+
 foreign import data WebSocketConnection :: Type
 
-newtype WebSocketMessage = WebSocketMessage String
+newtype WebSocketMessage
+  = WebSocketMessage String
+
 derive newtype instance showWSM :: Show WebSocketMessage
+
 derive instance newtypeWSM :: Newtype WebSocketMessage _
 
 -- | The effect associated with using the WebSocket module
@@ -23,177 +27,157 @@ foreign import data WS :: Effect
 
 -- TODO: more options from:
 -- https://github.com/websockets/ws/blob/master/doc/ws.md
-type WebSocketServerOptions =
-  ( host :: String
-  , backlog :: Int
-  )
+type WebSocketServerOptions
+  = ( host :: String
+    , backlog :: Int
+    )
 
 -- | The port to listen on if calling createWebSocketServerWithPort
-newtype Port = Port Int
+newtype Port
+  = Port Int
 
-newtype CloseCode = CloseCode Int
-newtype CloseReason = CloseReason String
+newtype CloseCode
+  = CloseCode Int
 
-foreign import createWebSocketServer_
-  :: forall e options
-   . EffFn2 (ws :: WS | e) options
-     (EffFn1 (ws :: WS | e) Unit Unit)
-     WebSocketServer
+newtype CloseReason
+  = CloseReason String
+
+foreign import createWebSocketServer_ ::
+  forall options.
+  EffectFn2 options
+    (EffectFn1 Unit Unit)
+    WebSocketServer
 
 -- | Creates a WebSocket.Server and internally a HTTP server
 -- | which binds to a given port
 -- |
 -- | The supplied callback is called when the created HTTP server
 -- | starts listening.
-createWebSocketServerWithPort
-  :: forall e options options' trash
-   . Union options options' WebSocketServerOptions
-  => RowLacks "port" options
-  => RowCons "port" Port options trash
-  => Port
-  -> { | options }
-  -> (Unit -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) WebSocketServer
-createWebSocketServerWithPort (Port port) options callback =
-  runEffFn2 createWebSocketServer_ options' callback'
-    where
-      options' = insert (SProxy :: SProxy "port") port options
-      callback' = mkEffFn1 callback
+createWebSocketServerWithPort ::
+  forall options options' trash.
+  Union options options' WebSocketServerOptions =>
+  Lacks "port" options =>
+  Cons "port" Port options trash =>
+  Port ->
+  { | options } ->
+  (Unit -> Effect Unit) ->
+  Effect WebSocketServer
+createWebSocketServerWithPort (Port port) options callback = runEffectFn2 createWebSocketServer_ options' callback'
+  where
+  options' = insert (SProxy :: SProxy "port") port options
+
+  callback' = mkEffectFn1 callback
 
 -- | Creates a WebSocket.Server from a pre-existing Node.Server
-createWebSocketServerWithServer
-  :: forall e options options' trash
-   . Union options options' WebSocketServerOptions
-  => RowLacks "server" options
-  => RowCons "server" Server options trash
-  => Server
-  -> { | options }
-  -> Eff (ws :: WS | e) WebSocketServer
-createWebSocketServerWithServer server options =
-  runEffFn2 createWebSocketServer_ options' callback'
-    where
-      options' = insert (SProxy :: SProxy "server") server options
-      callback' = mkEffFn1 $ const (pure unit)
+createWebSocketServerWithServer ::
+  forall options options' trash.
+  Union options options' WebSocketServerOptions =>
+  Lacks "server" options =>
+  Cons "server" Server options trash =>
+  Server ->
+  { | options } ->
+  Effect WebSocketServer
+createWebSocketServerWithServer server options = runEffectFn2 createWebSocketServer_ options' callback'
+  where
+  options' = insert (SProxy :: SProxy "server") server options
 
-foreign import onConnection_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketServer
-     (EffFn2 (ws :: WS | e) WebSocketConnection Request Unit)
-     Unit
+  callback' = mkEffectFn1 $ const (pure unit)
+
+foreign import onConnection_ ::
+  EffectFn2
+    WebSocketServer
+    (EffectFn2 WebSocketConnection Request Unit)
+    Unit
 
 -- | Attaches a connection event handler to a WebSocketServer
-onConnection
-  :: forall e
-   . WebSocketServer
-  -> (WebSocketConnection -> Request -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) Unit
-onConnection server callback =
-  runEffFn2 onConnection_ server (mkEffFn2 callback)
+onConnection ::
+  WebSocketServer ->
+  (WebSocketConnection -> Request -> Effect Unit) ->
+  Effect Unit
+onConnection server callback = runEffectFn2 onConnection_ server (mkEffectFn2 callback)
 
-foreign import onServerError_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketServer
-     (EffFn1 (ws :: WS | e) Error Unit)
-     Unit
+foreign import onServerError_ ::
+  EffectFn2
+    WebSocketServer
+    (EffectFn1 Error Unit)
+    Unit
 
 -- | Attaches an error event handler to a WebSocketServer
-onServerError
-  :: forall e
-   . WebSocketServer
-  -> (Error -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) Unit
-onServerError server callback =
-  runEffFn2 onServerError_ server (mkEffFn1 callback)
+onServerError ::
+  WebSocketServer ->
+  (Error -> Effect Unit) ->
+  Effect Unit
+onServerError server callback = runEffectFn2 onServerError_ server (mkEffectFn1 callback)
 
-foreign import onMessage_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketConnection
-     (EffFn1 (ws :: WS | e) WebSocketMessage Unit)
-     Unit
+foreign import onMessage_ ::
+  EffectFn2
+    WebSocketConnection
+    (EffectFn1 WebSocketMessage Unit)
+    Unit
 
 -- | Attaches a message event handler to a WebSocketConnection
-onMessage
-  :: forall e
-   . WebSocketConnection
-  -> (WebSocketMessage -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) Unit
-onMessage ws callback =
-  runEffFn2 onMessage_ ws (mkEffFn1 callback)
+onMessage ::
+  WebSocketConnection ->
+  (WebSocketMessage -> Effect Unit) ->
+  Effect Unit
+onMessage ws callback = runEffectFn2 onMessage_ ws (mkEffectFn1 callback)
 
-foreign import onClose_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketConnection
-     (EffFn2 (ws :: WS | e) CloseCode CloseReason Unit)
-     Unit
+foreign import onClose_ ::
+  EffectFn2
+    WebSocketConnection
+    (EffectFn2 CloseCode CloseReason Unit)
+    Unit
 
 -- | Attaches a close event handler to a WebSocketConnection
-onClose
-  :: forall e
-   . WebSocketConnection
-  -> (CloseCode -> CloseReason -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) Unit
-onClose ws callback =
-  runEffFn2 onClose_ ws (mkEffFn2 callback)
+onClose ::
+  WebSocketConnection ->
+  (CloseCode -> CloseReason -> Effect Unit) ->
+  Effect Unit
+onClose ws callback = runEffectFn2 onClose_ ws (mkEffectFn2 callback)
 
-foreign import onError_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketConnection
-     (EffFn1 (ws :: WS | e) Error Unit)
-     Unit
+foreign import onError_ ::
+  EffectFn2
+    WebSocketConnection
+    (EffectFn1 Error Unit)
+    Unit
 
 -- | Attaches an error event handler to a WebSocketConnection
-onError
-  :: forall e
-   . WebSocketConnection
-  -> (Error -> Eff (ws :: WS | e) Unit)
-  -> Eff (ws :: WS | e) Unit
-onError ws callback =
-  runEffFn2 onError_ ws (mkEffFn1 callback)
+onError ::
+  WebSocketConnection ->
+  (Error -> Effect Unit) ->
+  Effect Unit
+onError ws callback = runEffectFn2 onError_ ws (mkEffectFn1 callback)
 
-foreign import sendMessage_
-  :: forall e
-   . EffFn2 (ws :: WS | e)
-     WebSocketConnection
-     WebSocketMessage
-     Unit
+foreign import sendMessage_ ::
+  EffectFn2
+    WebSocketConnection
+    WebSocketMessage
+    Unit
 
 -- | Send a message over a WebSocketConnection
-sendMessage
-  :: forall e
-   . WebSocketConnection
-  -> WebSocketMessage
-  -> Eff (ws :: WS | e) Unit
-sendMessage ws message =
-  runEffFn2 sendMessage_ ws message
+sendMessage ::
+  WebSocketConnection ->
+  WebSocketMessage ->
+  Effect Unit
+sendMessage ws message = runEffectFn2 sendMessage_ ws message
 
-foreign import close_
-  :: forall e
-   . EffFn3 (ws :: WS | e)
-     WebSocketConnection
-     CloseCode
-     CloseReason
-     Unit
+foreign import close_ ::
+  EffectFn3
+    WebSocketConnection
+    CloseCode
+    CloseReason
+    Unit
 
 -- | Initiate a closing handshake
-close
-  :: forall e
-   . WebSocketConnection
-  -> Eff (ws :: WS | e) Unit
-close ws =
-  -- 1000 is the CloseCode for normal closure
-  runEffFn3 close_ ws (CloseCode 1000) (CloseReason "Closed by server")
+close ::
+  WebSocketConnection ->
+  Effect Unit
+close ws = runEffectFn3 close_ ws (CloseCode 1000) (CloseReason "Closed by server") -- 1000 is the CloseCode for normal closure
 
 -- | Initiate a closing handshake with given code and reason
-close'
-  :: forall e
-   . WebSocketConnection
-  -> CloseCode
-  -> CloseReason
-  -> Eff (ws :: WS | e) Unit
-close' ws code reason =
-  runEffFn3 close_ ws code reason
+close' ::
+  WebSocketConnection ->
+  CloseCode ->
+  CloseReason ->
+  Effect Unit
+close' ws code reason = runEffectFn3 close_ ws code reason
